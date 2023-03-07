bfeeb741f3b6ad94640d386b9e22a88d2fbea7d6
mcampbell379 <campbell.mitchell45@gmail.com>
2023-03-07 13:37:21 -0600

    added servlet bonuses

--- src/main/java/PageCounterServlet.java
+++ src/main/java/PageCounterServlet.java
@@ -2,9 +2,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Objects;
 
 @WebServlet(name = "PageCounterServlet", urlPatterns = "/count")
 public class PageCounterServlet extends HttpServlet {
@@ -12,6 +12,9 @@ public class PageCounterServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         count++;
+        if(Objects.equals(req.getParameter("reset"), "y")){
+            count = 0;
+        }
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
         out.println("<h1>Visit Count: " + count + "</h1>");
