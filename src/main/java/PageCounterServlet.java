62f75174cf1016d9039751ede7961b5f8cf95ba8
mcampbell379 <campbell.mitchell45@gmail.com>
2023-03-07 12:18:28 -0600

    added HelloWorldServlet and PageCounterServlet

--- src/main/java/PageCounterServlet.java
+++ src/main/java/PageCounterServlet.java
@@ -2,15 +2,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet(name = "PageCounterServlet", urlPatterns = "/count")
+public class PageCounterServlet extends HttpServlet {
+    private int count = 0;
 
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        count++;
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+        out.println("<h1>Visit Count: " + count + "</h1>");
     }
 }
