db74b6636cddc5ad5de40ad9226b640ad9d4d4f6
mcampbell379 <campbell.mitchell45@gmail.com>
2023-03-16 09:19:57 -0500

    save point

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -18,20 +18,11 @@ public class LoginServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
 
         // TODO: find a record in your database that matches the submitted password
         // TODO: make sure we find a user with that username
         // TODO: check the submitted password against what you have in your database
-        boolean validAttempt = username.equals("admin") && password.equals("password");
 
-        if (validAttempt) {
             // TODO: store the logged in user object in the session, instead of just the username
-            request.getSession().setAttribute("user", username);
-            response.sendRedirect("/profile");
-        } else {
-            response.sendRedirect("/login");
-        }
     }
 }
